1b140316fe04ce11fc16f0bbabac1c8fd877caca
Jacktone Clarance <77950647+claranceJ@users.noreply.github.com>
2023-04-17 10:14:58 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+import random
+
+print("Totally Random one-Million-to-one")
+print()
+count = 1
+my_number = random.randint(1,100)
+while True:
+    guess = int(input("What is your guess?: "))
+    if guess< my_number:
+        count += 1
+        print("Too low")
+    elif guess > my_number:
+        count += 1
+        print("Too high")
+    elif guess== my_number:
+        count += 1
+        print("Correct!")
+        print()
+        print(f"It took you {count} guesses to get the correct number")
+        print()
+        play=input("Type 'run' to try again with a different number: ")
+        print()
+        if play!="run":
+          break
+        else:
+          my_number=random.randint(1,100)
+          count=1
+          continue
+
